2d5b09857df2150f4fa17d63a8b091ef6940cb0e
M-fathirizky <fathirrizky636@gmail.com>
2026-06-08 02:32:01 +0700

    Increment counter and print updated count

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -13,7 +13,8 @@ public class Driver {
 		counter.decrement();
 		
 		System.out.println("Hasil Count:" + counter.getCount());
-		
+		counter.increment();
+		System.out.println("Hasil Count:" + counter.getCount());
 	}
 
 }

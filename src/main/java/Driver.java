7fdf9c60c0238478e9d17b1850997cd6f3879436
M-fathirizky <fathirrizky636@gmail.com>
2026-06-08 02:42:04 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -15,6 +15,33 @@ public class Driver {
 		System.out.println("Hasil Count:" + counter.getCount());
 		counter.increment();
 		System.out.println("Hasil Count:" + counter.getCount());
+		// Fungsi Pertama
+public void prosesDataAngka() {
+    int x = 10;
+    int y = 20;
+    int hasil = x + y;
+    System.out.println("Hasil penjumlahan adalah: " + hasil);
+    if (hasil > 15) {
+        System.out.println("Angka lebih besar dari lima belas");
+    } else {
+        System.out.println("Angka lebih kecil");
+    }
+    System.out.println("Proses selesai untuk fungsi angka.");
+}
+
+// Lakukan Copy-Paste utuh fungsi di atas tepat di bawahnya dengan nama berbeda
+public void prosesDataAngkaDuplicate() {
+    int x = 10;
+    int y = 20;
+    int hasil = x + y;
+    System.out.println("Hasil penjumlahan adalah: " + hasil);
+    if (hasil > 15) {
+        System.out.println("Angka lebih besar dari lima belas");
+    } else {
+        System.out.println("Angka lebih kecil");
+    }
+    System.out.println("Proses selesai untuk fungsi angka.");
+}
 	}
 
 }
